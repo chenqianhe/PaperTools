ece81da7a0aef7657f0818a2f8c7f0f7ab59185a
chenqianhe <1278095698@qq.com>
2023-02-07 22:04:11 +0800

    :sparkles: add doc

--- src/views/DocsView.vue
+++ src/views/DocsView.vue
@@ -23,6 +23,12 @@ const contents = [
     intro:
       "本文为 MathJax 在 Cmd Markdown 环境下的常用语法指引。Cmd Markdown 编辑阅读器支持 Latex 编辑显示支持",
   },
+  {
+    name: "Latex 入门",
+    website: "https://zhuanlan.zhihu.com/p/456055339",
+    intro:
+      "本份教程适合完全没有用过LaTeX的读者使用，学习完后应该能满足LaTeX日常使用的要求。如果之前已经使用过LaTeX的话，也可以参考本文学习",
+  },
 ];
 </script>
 
